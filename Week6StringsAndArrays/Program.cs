4ec642c980f13c9e32d0df03aadff66990ba46dc
Mattias Kitvel <mkitvel@tktk.ee>
2023-03-05 14:04:25 +0200

    add week6 code

---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,18 @@
+﻿//
+string hello = " we are the champions?";
+
+int stringLenght = hello.Length;
+hello= hello.Trim();
+char firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?','!');
+firstLetter= hello[0];
+Console.WriteLine(firstLetter);
+Console.WriteLine(hello);   
+//string trimmedString = hello.Trim();
+
+
+//int wordCounter = 0;
+//for (int i = 0; i < hello.Length; i++)
+//{ if (char.IsWhiteSpace(hello[i])) { wordCounter++; } }
+//Console.WriteLine(hello.Length);
